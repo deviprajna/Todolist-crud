af5e0662a4eb69cd260a1ea38d98f233356f647f
Devi prajna <deviparamita93@gmail.com>
2026-05-28 12:39:23 +0700

    main

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const connectDB = require('./config/db.js');
 const router = require('./route/task_router.js');
@@ -11,8 +12,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res)=> {
+  res.sendFile(path.join(__dirname, 'public', "index.html"))
+});
 app.use('/api/tasks', router);
 
 connectDB();
